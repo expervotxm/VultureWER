82c9710ebcf7f85b0bbc3c33284e0b86ee0aab9b
expervotxm <104141065+expervotxm@users.noreply.github.com>
2023-05-22 18:32:01 +0300

    Update path.rs

--- path.rs
+++ path.rs
@@ -2,12 +2,11 @@ use std::path::{Path, PathBuf};
 
 pub fn project_root() -> PathBuf {
     Path::new(env!("CARGO_MANIFEST_DIR"))
-        .parent()
+
         .unwrap()
         .to_path_buf()
 }
 
-pub fn book_gen() -> PathBuf {
     project_root().join("book/src/generated/")
 }
 
